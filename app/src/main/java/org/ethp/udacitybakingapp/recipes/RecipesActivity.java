df1036ff7c1e30d89c356fb4b925e016412a2e19
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2018-07-27 00:23:54 -0500

    Start project

---
+++ app/src/main/java/org/ethp/udacitybakingapp/recipes/RecipesActivity.java
@@ -0,0 +1,15 @@
+package org.ethp.udacitybakingapp.recipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.ethp.udacitybakingapp.R;
+
+public class RecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
